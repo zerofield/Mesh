327b5ecc88046f3b90e391ecb28fd5ddbe47a87a
Jin Xu <xujin@i-vsun.com>
2016-06-27 14:02:33 +0800

    modified ground script

--- Assets/Scripts/GroundClick.cs
+++ Assets/Scripts/GroundClick.cs
@@ -1,15 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GroundClick : MonoBehaviour {
+public class GroundClick : MonoBehaviour
+{
+    public Material matRoad;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void OnMouseDown()
+    {
+        //Create a road
+        GameObject road = new GameObject("road", typeof(MeshFilter), typeof(MeshRenderer));
+        road.transform.position = new Vector3(0, 0.1f, 0);
+
+        MeshFilter meshFilter = road.GetComponent<MeshFilter>();
+        MeshRenderer meshRenderer = road.GetComponent<MeshRenderer>();
+        //Create a mesh
+        Mesh mesh = new Mesh();
+        meshFilter.mesh = mesh;
+
+        Vector3[] vertices =
+        {
+            new Vector3(0,0,0),
+            new Vector3(1,0,0),
+            new Vector3(1,0,1),
+            new Vector3(0,0,1),
+
+        };
+
+        int[] triangles =
+        {
+            0, 2, 1,
+            3, 2, 0
+        };
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+
+        meshRenderer.material = matRoad;
+    }
 }
