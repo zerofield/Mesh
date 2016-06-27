813253790bf490fc43fe35cb9a1f90f25218f730
Jin <zerofield@outlook.com>
2016-06-27 22:28:57 +0800

    video 6

--- Assets/Scripts/GroundClick.cs
+++ Assets/Scripts/GroundClick.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class GroundClick : MonoBehaviour
 {
-    public Material matRoad;
+    public GameObject prefabRoad;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
 
-    void OnMouseDown()
+            if (GetComponent<Collider>().Raycast(ray, out hit, Mathf.Infinity))
+            {
+
+                CreateRoad(hit.point);
+            }
+        }
+    }
+
+    void CreateRoad(Vector3 point)
     {
         //Create a road
-        GameObject road = new GameObject("road", typeof(MeshFilter), typeof(MeshRenderer));
-        road.transform.position = new Vector3(0, 0.1f, 0);
+        GameObject road = Instantiate(prefabRoad, point + new Vector3(0, 0.1f, 0), Quaternion.identity) as GameObject;
 
         MeshFilter meshFilter = road.GetComponent<MeshFilter>();
         MeshRenderer meshRenderer = road.GetComponent<MeshRenderer>();
@@ -17,12 +30,15 @@ public class GroundClick : MonoBehaviour
         Mesh mesh = new Mesh();
         meshFilter.mesh = mesh;
 
+        float width = 1f;
+        float length = 5f;
+
         Vector3[] vertices =
         {
-            new Vector3(0,0,0),
-            new Vector3(1,0,0),
-            new Vector3(1,0,1),
-            new Vector3(0,0,1),
+            new Vector3(0       ,0,-width/2),
+            new Vector3(length  ,0,-width/2),
+            new Vector3(length  ,0,width/2),
+            new Vector3(0       ,0,width/2),
 
         };
 
@@ -32,13 +48,23 @@ public class GroundClick : MonoBehaviour
             3, 2, 0
         };
 
+        Vector2[] uv =
+        {
+            new Vector2(0,0),
+            new Vector2(length,0),
+            new Vector2(length,1),
+            new Vector2(0,1),
+        };
+
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+        mesh.uv = uv;
 
         mesh.Optimize();
         mesh.RecalculateNormals();
         mesh.RecalculateBounds();
 
-        meshRenderer.material = matRoad;
+        // Vector2 texScale = new Vector2(5, 1);
+        // road.GetComponent<MeshRenderer>().materials[0].mainTextureScale = texScale;
     }
 }
